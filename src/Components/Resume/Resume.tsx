f51410358cc41a4a196fcb8d50007b34fb6fcee8
mridul-22 <mridulrabha309@gmail.com>
2024-10-21 12:46:03 +0530

    Added Resume link

--- src/Components/Resume/Resume.tsx
+++ src/Components/Resume/Resume.tsx
@@ -4,7 +4,7 @@ import { CiLink } from "react-icons/ci";
 const Resume = () => {
     return (
         <div className={styles.container}>
-            <a href="https://bit.ly/kherkataryresume">
+            <a href="https://drive.google.com/file/d/1zL7xA7RwTUSOj5Rp02elkgULmd_jM78n/view?usp=sharing">
 
             
             <div className={styles.btn}>Resume <div> <CiLink /> </div> </div>
